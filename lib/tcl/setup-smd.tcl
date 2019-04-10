2c921b48f1e8843967991886007179de2cd36afc
peffranckpeffan <peffanfranck@gmail.com>
2019-04-10 20:02:14 -0300

    back to original restraints -- without side

--- lib/tcl/setup-smd.tcl
+++ lib/tcl/setup-smd.tcl
@@ -7,14 +7,12 @@ $all set occupancy 0.0
 $all writepdb refumb0.pdb
 set prot [atomselect top "segname A and backbone"]
 $prot set occupancy 1.0
-set protr [atomselect top "segname A and noh and same residue as within 6 of segname B"]
-$protr set beta 3.0
 measure center $prot weight mass
 set am [atomselect top "segname B and backbone"]
 $am set occupancy 2.0
 measure center $am weight mass
-set amr [atomselect top "segname B and noh"]
-$amr set beta 4.0
+set am [atomselect top "segname B and noh"]
+$am set beta 3.0
 $all writepdb atoms.pdb
 
 exit
